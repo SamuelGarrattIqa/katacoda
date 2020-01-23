e0ec7c86fad1c1d1d7f5960bd00a49738dddf07c
Samuel Garratt <samuel.garratt@integrationqa.com>
2020-01-23 22:25:47 +0000

    Say yes to prompts

--- specflow_api_testing/install.sh
+++ specflow_api_testing/install.sh
@@ -3,6 +3,6 @@ apt-get install wget
 wget -q https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 apt-get update
-apt-get install apt-transport-https
+apt-get install -y apt-transport-https
 apt-get update
-apt-get install dotnet-sdk-3.1
+apt-get install -y dotnet-sdk-3.1
